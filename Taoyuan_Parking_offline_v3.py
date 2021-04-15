0066941615adb7a61944eee22790c11cd7c2479d
yachuuu <word123488@yahoo.com.tw>
2021-04-15 09:40:43 +0800

    Revert "Version4"
    This reverts commit c21ce604b4cc5c0db1b072e43b6e46b422b58a38.

--- Taoyuan_Parking_offline_v3.py
+++ Taoyuan_Parking_offline_v3.py
@@ -10,8 +10,6 @@ https://data.gov.tw/dataset/25940
 https://data.tycg.gov.tw/opendata/datalist/datasetMeta/download?id=f4cc0b12-86ac-40f9-8745-885bddc18f79&rid=0daad6e6-0632-44f5-bd25-5e1de1e9146f
 """
 
-print('Version4')
-
 import json
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties  # 步驟一
